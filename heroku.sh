f1585894261f730c8b3e77952da6159103d15139
North McCormick <northmccormick@icloud.com>
2017-02-17 18:00:49 -0800

    +

--- heroku.sh
+++ heroku.sh
@@ -6,5 +6,5 @@
 #echo "Installing Cordova"
 #npm install cordova --verbose >&2
 
-echo "Serving with --lab --port 80 --nolivereload"
-ionic serve --lab --port 1024 --nolivereload --nogulp >&2
+echo "Serving with --lab --port $PORT --nolivereload"
+ionic serve --lab --port $PORT --nolivereload --nogulp  >&2

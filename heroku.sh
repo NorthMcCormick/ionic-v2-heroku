a8cf672d45002962807553a1acdebd6048ed44a5
North McCormick <northmccormick@icloud.com>
2017-02-17 17:28:20 -0800

    + Split the installs so that it doesn’t timeout

--- heroku.sh
+++ heroku.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-echo "Installing Ionic and Cordova"
-npm install ionic cordova --verbose >&2
+echo "Installing Ionic"
+npm install ionic --verbose >&2
+
+echo "Installing Cordova"
+npm install cordova --verbose >&2
 
 echo "Serving with --lab"
 ionic serve --lab >&2

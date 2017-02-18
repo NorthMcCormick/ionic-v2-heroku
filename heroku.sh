9e853b994ad7266b38296272f12de70fd7367f32
North McCormick <northmccormick@icloud.com>
2017-02-17 17:31:42 -0800

    + Add ionic to the dependencies (maybe will keep it from timing out)

--- heroku.sh
+++ heroku.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-echo "Installing Ionic"
-npm install ionic --verbose >&2
+#echo "Installing Ionic"
+#npm install ionic --verbose >&2
 
-echo "Installing Cordova"
-npm install cordova --verbose >&2
+#echo "Installing Cordova"
+#npm install cordova --verbose >&2
 
 echo "Serving with --lab"
 ionic serve --lab >&2
